0a99349fcf426c482b04d411ef28584b16c45c5b
KhaDaniil <43844276+KhaDaniil@users.noreply.github.com>
2018-11-28 18:39:18 +0300

    Add files via upload

---
+++ 25.11.18/N1/N1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ for (int y = 0; y <= 18; y++) {
+ cout << (78487894 - 2019 * (1820 + 2018 * y))/2018 << "  " << ((y * 2018) + 1820) << endl;
+ }
+ return 0;
+}
